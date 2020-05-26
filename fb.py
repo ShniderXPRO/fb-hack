87c2070373620a5a7329860c0b103bcb5f6b28af
ShniderXPRO <65216448+ShniderXPRO@users.noreply.github.com>
2020-05-26 07:30:47 -0500

    Add files via upload

---
+++ fb.py
@@ -0,0 +1,24 @@
+import time
+import os
+os.system("clear")
+time.sleep (5)
+print ("Please Wait....")
+time.sleep (5)
+print ("Please Wait....")
+time.sleep (2)
+os.system ("clear")
+time.sleep (2)
+os.system("figlet FB-CRACK")
+xp = input ("\033[1;33mEnter Password Tool:\033[1;37m ")
+if xp == ("ddd221") :
+	time.sleep (5)
+	xo = input ("\033[1;33mEnter Email ID or Phone Number:\033[1;37m ")
+	time.sleep (5)
+	print ("\033[1;32mGet info....")
+	time.sleep (6)
+	print ("\033[1;37mStart Cracking......")
+	time.sleep (15)
+	print ("\033[1;32mPASSWORD \033[1;33m>>>>>>>>>> \033[1;37m0102030405060 ")
+else :
+		time.sleep (5)
+		print ("\033[1;31mWrong Password")
